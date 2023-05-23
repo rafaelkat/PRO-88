e5ee9135c9553b23b9dc43ddf43c5dde6a97abe7
rafaelkat <97891367+rafaelkat@users.noreply.github.com>
2023-05-23 15:15:56 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCC-vTH-oP3SFJMo0dua_SySPHJ34-A81A",
+    authDomain: "pro-85-f64f7.firebaseapp.com",
+    databaseURL: "https://pro-85-f64f7-default-rtdb.firebaseio.com",
+    projectId: "pro-85-f64f7",
+    storageBucket: "pro-85-f64f7.appspot.com",
+    messagingSenderId: "371399909683",
+    appId: "1:371399909683:web:375c91a6d6c0b8342f5e46"
+  };
